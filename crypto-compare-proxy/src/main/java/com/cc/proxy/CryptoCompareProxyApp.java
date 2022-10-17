5cbf43ecc41ad79ce19a5b3f3601c820eef74b82
hezida <hezi.daniel@sap.com>
2022-10-17 23:28:29 +0300

    first commit

---
+++ crypto-compare-proxy/src/main/java/com/cc/proxy/CryptoCompareProxyApp.java
@@ -0,0 +1,19 @@
+package com.cc.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource({ "classpath:/application.properties", "classpath:/rest.properties", "classpath:/cache.properties", "classpath:admin-key.properties" })
+public class CryptoCompareProxyApp /*extends SpringBootServletInitializer*/ {
+
+	/**
+	 * The main entry point into this spring boot application
+	 * @param args the array of string arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCompareProxyApp.class, args);
+	}
+
+}
